708a64de78d8b17def8ad2f82c0b25a1637bed53
Ayshe Sammyeva <a.sammyeva@ussat.tm>
2023-02-28 11:22:53 +0500

    2.3.1

--- fromfile.py
+++ fromfile.py
@@ -11,48 +11,48 @@ def fromFile(folder):
         return None
 
     files = os.listdir(filePath)
-    for levelNumber in range(0, len(files)):
+    for _ in range(0, len(files)):
         versionParent = 1
         versionChild = 1
         stepNumber = 1
-        newFunc(levelNumber,versionParent,versionChild,stepNumber,files, filePath)
+        newFunc(versionParent,versionChild,stepNumber,files, filePath)
 
 
 
-def newFunc(levelNumber,versionParent,versionChild,stepNumber,files, filePath):
-    currentFile = f'{levelNumber}_v{versionParent}.{versionChild}_s{stepNumber}_'
+def newFunc(versionParent,versionChild,stepNumber,files, filePath):
+    currentFile = f'_v{versionParent}.{versionChild}_s{stepNumber}_'
     print(currentFile)
 
     for file in files:
         fileVars = os.path.splitext(file)
         fileExt = fileVars[-1]
         fileName = os.path.basename(fileVars[0])
-        print(fileVars, fileName.startswith(currentFile))
+        print(fileVars, currentFile in fileName)
         # found file
-        if fileName.startswith(currentFile):
+        if currentFile in fileName:
+            if fileName[0] == '1':
+                # found type file
+                for fileType, v in EXTENSIONS.items():
+                    print(fileType)
+                    if fileExt in v:
+                        print(v)
+                        vals = fileName.split('_')
+                        duration = int(vals[3][1:])/ 10
+                        coordination = tuple(int(r) for r in vals[4][1:].split(' ')) if vals[4][1:] != '' else None
+                        print(coordination)
 
-            # found type file
-            for fileType, v in EXTENSIONS.items():
-                print(fileType)
-                if fileExt in v:
-                    print(v)
-                    vals = fileName.split('_')
-                    duration = int(vals[3][1:])/ 10
-                    coordination = tuple(int(r) for r in vals[4][1:].split(' ')) if vals[4][1:] != '' else None
-                    print(coordination)
+                        if fileType == 'img':
+                            status = compareColoredImg(os.path.join(filePath, file), duration, coordination)
 
-                    if fileType == 'img':
-                        status = compareColoredImg(os.path.join(filePath, file), duration, coordination)
+                        elif fileType == 'txt':
+                            status = textFunc(os.path.join(filePath, file), duration, coordination)
 
-                    elif fileType == 'txt':
-                        status = textFunc(os.path.join(filePath, file), duration, coordination)
-
-            if status:
-                versionParent = versionChild
-                stepNumber += 1
-                newFunc(levelNumber,versionParent,versionChild,stepNumber,files, filePath)
-            else:
-                versionChild += 1
-                newFunc(levelNumber,versionParent,versionChild,stepNumber,files, filePath)
+                if status:
+                    versionParent = versionChild
+                    stepNumber += 1
+                    newFunc(versionParent,versionChild,stepNumber,files, filePath)
+                else:
+                    versionChild += 1
+                    newFunc(versionParent,versionChild,stepNumber,files, filePath)
 
             
